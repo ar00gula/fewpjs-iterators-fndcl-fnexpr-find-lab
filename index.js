72003ef37a899a60d074607e34c860113036c8d8
ar00gula <arianna.mayer@gmail.com>
2021-01-18 12:37:30 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(arrayOfObjects) {
+  const winner = arrayOfObjects.find( function(object) { return object.result === "W" })
+    if (winner) {
+      return winner.year
+    }
+}
+
+superbowlWin(record)
